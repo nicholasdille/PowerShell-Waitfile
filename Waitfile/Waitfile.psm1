6a706b90a198fd2ab06ac90b792b8098291b9c4c
Nicholas Dille <nicholas.dille@mailbox.org>
2018-01-23 21:39:08 +0100

    Fix for importing functions

--- Waitfile/Waitfile.psm1
+++ Waitfile/Waitfile.psm1
@@ -3,3 +3,8 @@ $script:Types = @{}
 
 # Data structure for storing targets
 $script:Targets = @{}
+
+# Import functions
+Get-ChildItem -Path "$PSScriptRoot\Functions" -Filter '*.ps1' -Recurse | ForEach-Object {
+    . "$($_.FullName)"
+}
