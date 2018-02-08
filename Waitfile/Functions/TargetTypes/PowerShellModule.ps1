68cafd7a80659784bfcfe876a3c51ba7ca585a66
Nicholas Dille <nicholas.dille@mailbox.org>
2018-02-08 22:50:49 +0100

    Added new target type

---
+++ Waitfile/Functions/TargetTypes/PowerShellModule.ps1
@@ -0,0 +1,15 @@
+New-TargetType `
+    -Name 'PowerShellModule' `
+    -Arguments 'Name', 'Version' `
+    -Test { `
+            param([hashtable]$Arguments) `
+            [bool](Get-Module -ListAvailable -Name $($Arguments.Name)) `
+        } `
+    -New { `
+            param([hashtable]$Arguments) `
+            Install-Module -Name $($Arguments.Name) -RequiredVersion $($Arguments.Version) -Scope CurrentUser `
+        } `
+    -Remove { `
+            param([hashtable]$Arguments) `
+            Uninstall-Module -Name $(Arguments.Name) -RequiredVersion $($Arguments.Version) `
+        }
