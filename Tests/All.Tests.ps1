b78f909ede2d179bb64ea1e7c0aa3f8162e0d6e0
Nicholas Dille <nicholas.dille@mailbox.org>
2018-02-11 23:12:40 +0100

    Fixed tests

--- Tests/All.Tests.ps1
+++ Tests/All.Tests.ps1
@@ -4,34 +4,51 @@ Import-Module -Name Waitfile -Force
 
 Describe 'Makefile' {
     It 'Add new type' {
-        $Count = $Types.Keys.Count + 1
+        $Count = @(Get-TargetType).Count + 1
         New-TargetType -Name 'File' -Test {} -New {} -Remove {}
-        $Types.Keys.Count | Should Be $Count
+        @(Get-TargetType).Count | Should Be $Count
     }
     It 'Fails to add existing type' {
-        $Count = $Types.Keys.Count
+        $Count = @(Get-TargetType).Count
         { New-TargetType -Name 'File' -Test {} -New {} -Remove {} } | Should Throw
-        $Types.Keys.Count | Should Be $Count
+        @(Get-TargetType).Count | Should Be $Count
     }
     It 'Adds new target' {
-        $Count = $Targets.Keys.Count + 1
-        New-Target -Name 'Test1' -Type 'File' -Action {}
-        $Targets.Keys.Count | Should Be $Count
+        $Count = @(Get-Target).Count + 1
+        New-Target -Name 'Test1' -Type 'File'
+        @(Get-Target).Count | Should Be $Count
     }
-    It 'Adds second non-default target' {
-        $Count = $Targets.Keys.Count + 1
+    It 'Adds second target' {
+        $Count = @(Get-Target).Count + 1
         New-Target -Name 'Test2' -Type 'File' -DependsOn 'Test1' -Default
-        $Targets.Keys.Count | Should Be $Count
+        @(Get-Target).Count | Should Be $Count
     }
     It 'Fails to add existing target' {
-        $Count = $Targets.Keys.Count
+        $Count = @(Get-Target).Count
         { New-Target -Name 'Test2' -Type 'File' } | Should Throw
-        $Targets.Keys.Count | Should Be $Count
+        @(Get-Target).Count | Should Be $Count
     }
     It 'Fails to add on non-existent dependency' {
         { New-Target -Name 'Test3' -Type 'File' -DependsOn 'Test0' } | Should Throw
     }
     It 'Fails to add two default targets' {
         { New-Target -Name 'Test3' -Type 'File' -Default } | Should Throw
+    }
+    It 'Invokes a target' {
+        function Test-InvokeTargetTest {}
+        function Test-InvokeTargetNew {}
+        function Test-InvokeTargetRemove {}
+        Mock Test-InvokeTargetTest {}
+        Mock Test-InvokeTargetNew {}
+        Mock Test-InvokeTargetRemove {}
+        Clear-Target
+        New-TargetType -Name 'TargetTest' -Test { Test-InvokeTargetTest } -New { Test-InvokeTargetNew } -Remove { Test-InvokeTargetRemove }
+        New-Target -Name 'TargetTest' -Type 'TargetTest' -Default
+        Invoke-Target -Action New
+        Assert-MockCalled -CommandName Test-InvokeTargetTest -Times 1 -Exactly
+        Assert-MockCalled -CommandName Test-InvokeTargetNew -Times 1 -Exactly
+        Invoke-Target -Action Remove
+        Assert-MockCalled -CommandName Test-InvokeTargetTest -Times 2 -Exactly
+        Assert-MockCalled -CommandName Test-InvokeTargetRemove -Times 1 -Exactly
     }
 }
